8ef2ddc0c93251df0b810f8d6aab5a686779252e
uzairsaqib35 <uzair_saqib35@yahoo.com>
2021-11-30 01:41:06 -0800

    Stack using Linked List started
    Stack using Linked List started

---
+++ ConsoleApp1/LinkedList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class LinkedList
+    {
+        public int value { get; set; }
+        public LinkedList nextNode { get; set; }
+
+        public bool isEmpty()
+        {
+            return this == null ? true : false;
+        }
+
+        public LinkedList insertAtTheBeginningOfTheList(int value)
+        {
+            LinkedList newNode = new LinkedList();
+            newNode.value = value;
+
+            if (isEmpty())
+            {
+                newNode.nextNode = null;
+                return newNode;
+            }
+            else 
+            {
+                return newNode;
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/StackWithLinkedList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class StackWithLinkedList
+    {
+        private int value { get; set; }
+        private StackWithLinkedList nextNode { get; set; }
+
+
+        public bool isEmpty(StackWithLinkedList firstNode) 
+        {
+            return firstNode == null ? true:false;
+        }
+
+        public void push(int value) 
+        {
+            //Here push means inserting a node at the beginning of the linked list
+            StackWithLinkedList newNode = new StackWithLinkedList();
+            newNode.value = value;
+
+            if (isEmpty(this)) 
+            {
+            
+            }
+
+        }
+
+
+
+    }
+}
