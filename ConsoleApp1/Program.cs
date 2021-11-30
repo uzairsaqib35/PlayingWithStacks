00c616817e83267fab8d15d74245672831c7af2a
uzairsaqib35 <uzair_saqib35@yahoo.com>
2021-11-30 01:26:23 -0800

    Stack done and tested with arrays
    Stack done and tested with arrays

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Testing stack with arrays
+            StackWithArrays swa = new StackWithArrays(50);
+            swa.push(1);
+            swa.push(2);
+            swa.push(3);
+            swa.push(4);
+            swa.push(5);
+
+            swa.peek();
+
+            swa.printStack();
+
+            swa.pop();
+            swa.pop();
+            swa.pop();
+
+            swa.printStack();
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ConsoleApp1/StackWithArrays.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class StackWithArrays
+    {
+        int top = -1;
+        int[] stk;
+        int MAX = 0;
+
+        public StackWithArrays(int max) 
+        {
+            this.MAX = max;
+            stk = new int[MAX];
+        }
+
+        public bool isEmpty() 
+        {
+            return this.top == -1 ? true : false;
+        }
+
+        public bool isFull() 
+        {
+            return this.top == MAX - 1 ? true:false;
+        }
+
+        public void printStack() 
+        {
+            if (!isEmpty())
+            {
+                for (int i = top; i >= 0; i--)
+                {
+                    Console.WriteLine("|"+stk[i]+ "|");
+                }
+
+            }
+            else 
+            {
+                Console.WriteLine("Can't print stack is empty");
+            }
+            
+        }
+
+        public void peek() 
+        {
+            if (!isEmpty())
+            {
+                Console.WriteLine("PEEK CALLED : =>" + stk[top]);
+            }
+            else 
+            {
+                Console.WriteLine("CAN'T PEEK STACK IS EMPTY");
+            }
+          
+        }
+
+        public void push(int value) 
+        {
+            if (!isFull())
+            {
+                top++;
+                stk[top]=value;
+
+                Console.WriteLine("("+value+")PUSHED TO STACK");
+
+            }
+            else 
+            {
+                Console.WriteLine("CAN'T PUSH STACK IS FULL");
+            }
+        }
+
+        public void pop()
+        {
+            if (!isEmpty())
+            {
+                int temp = stk[top];
+                top--;
+
+                Console.WriteLine("(" + temp + ")POPED FROM STACK");
+            }
+            else
+            {
+                Console.WriteLine("CAN'T POP STACK IS EMPTY");
+            }
+        }
+
+    }
+}
